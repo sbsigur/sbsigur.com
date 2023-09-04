9dfbeb58a7cd25516e4b5513daaf28b8e603885b
Sævar Bergur Sigurgeirsson <saevar@ok.is>
2023-09-04 17:38:59 +0000

    Update website layout and functionality for improved user experience
    Major overhaul of the website's layout and formatting according to user feedback and analytics. This includes updating the CSS, restructuring HTML, and adding new JavaScript functionalities. Styles are now more responsive and user-friendly, with dark-mode toggle and better mobile view support. JavaScript was added to increase interactivity and engage user attention. Changes also support better website analytics for informed UI improvements in the future.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Welcome to my personal website!");
+document.addEventListener("DOMContentLoaded", function() {
+    const darkModeToggle = document.getElementById("darkModeToggle");
+
+    darkModeToggle.addEventListener("click", function() {
+        document.body.classList.toggle("dark-mode");
+        const h2Elements = document.querySelectorAll("h2");
+        h2Elements.forEach(h2 => h2.classList.toggle("dark-mode"));
+    });
+});
