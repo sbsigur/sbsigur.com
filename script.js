61298510fa048eebf7c3c70039a1c761c1c7ea92
Sævar Bergur Sigurgeirsson <saevar@ok.is>
2023-09-04 18:23:29 +0000

    Add favicon and improve readability in script.js
    Added a favicon to the index.html for better brand recognition and consistency across tabs. In script.js, modified event listener syntax for better readability and alignment with coding standards.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const darkModeToggle = document.getElementById("darkModeToggle");
 
-    darkModeToggle.addEventListener("click", function() {
+    darkModeToggle.addEventListener("click", function () {
         document.body.classList.toggle("dark-mode");
         const h2Elements = document.querySelectorAll("h2");
         h2Elements.forEach(h2 => h2.classList.toggle("dark-mode"));
