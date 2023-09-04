ea33c8000402e5c3d332251b01c7d070f3ee31d8
Sævar Bergur Sigurgeirsson <saevar@ok.is>
2023-09-04 14:19:01 +0000

    Next.JS

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <main style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            textAlign: 'center'
+        }}>
+            <h1>Welcome to my personal website!</h1>
+        </main>
+    );
+}
