d6973c481dc7d23d6a63c8b7b263aadb8825e67c
Sævar Bergur Sigurgeirsson <saevar@ok.is>
2023-09-04 14:58:23 +0000

    Update website layout and content
    The layout and content of the website have been completely revamped. The previous version was more focused on jokes and memes, however, the new version is turned into a personal portfolio. This allows the user to showcase their work and provide information about themselves. The CSS file has been updated to change the look and feel of the website to be cleaner and more professional. All joke-related content has been removed from the HTML and Javascript files and replaced with standard portfolio sections such as 'About', 'Projects', and 'Contact'.

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const quoteButton = document.getElementById("quoteButton");
-    const quoteText = document.getElementById("quoteText");
-
-    const fetchQuote = async () => {
-        const response = await fetch('https://api.quotable.io/random');
-        const data = await response.json();
-        return `${data.content} —${data.author}`;
-    };
-
-    quoteButton.addEventListener("click", async function() {
-        const quote = await fetchQuote();
-        quoteText.textContent = quote;
-    });
-});
+console.log("Welcome to my personal website!");
