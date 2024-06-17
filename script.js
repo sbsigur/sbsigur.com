bbc969afb143a041d19bd828bf65b60f624de247
Sævar Bergur Sigurgeirsson <sbsigur@icloud.com>
2024-06-17 15:22:19 +0000

    Simple resume site.

--- script.js
+++ script.js
@@ -3,16 +3,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         document.body.classList.add("dark-mode");
-        const h2Elements = document.querySelectorAll("h2");
-        h2Elements.forEach(h2 => h2.classList.add("dark-mode"));
         darkModeToggle.textContent = "Light Mode";
     }
 
     darkModeToggle.addEventListener("click", function () {
         document.body.classList.toggle("dark-mode");
-        const h2Elements = document.querySelectorAll("h2");
-        h2Elements.forEach(h2 => h2.classList.toggle("dark-mode"));
-
         if (document.body.classList.contains("dark-mode")) {
             darkModeToggle.textContent = "Light Mode";
         } else {
